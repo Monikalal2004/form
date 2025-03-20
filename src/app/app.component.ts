ba70d6bf65f74d143a00e5a6d86e26ef493b976f
unknown <Monikalal>
2025-03-20 18:46:23 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DispatchComponent } from './dispatch/dispatch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DispatchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
